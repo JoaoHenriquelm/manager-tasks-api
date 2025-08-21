a1e43b26c01457b6d96724c41e6cbd3e7fef06bc
JoaoHenriquelm <gitjao@gmail.com>
2025-08-21 02:22:30 -0300

    initial

---
+++ src/main/java/com/example/managertasks/ManagertasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.managertasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagertasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagertasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/managertasks/ManagertasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managertasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagertasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
